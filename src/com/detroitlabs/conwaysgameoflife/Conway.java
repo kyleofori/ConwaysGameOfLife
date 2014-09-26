07573f3f43a69d285b0ec2efcc2609ff98facab5
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-09-26 17:34:52 -0400

    Refactoring Conway to deal with grids being composed of cells instead of booleans

--- src/com/detroitlabs/conwaysgameoflife/Conway.java
+++ src/com/detroitlabs/conwaysgameoflife/Conway.java
@@ -20,11 +20,11 @@ public class Conway {
         int t,i;
         for(t=0; t<5; t++) {
             for (i=0; i<5; i++) {
-                if(grid.getGrid()[t][i]) {
+                if(grid.getGrid()[t][i].getAlive()) {
                     if(/*has 0, 1, or 4-8 living neighbors*/)
                     {
                         //cell should die
-                        grid.getGrid()[t][i] = false;
+                        grid.getGrid()[t][i].setAlive(false);
                     } //otherwise the cell stays alive
                 }
                 else
@@ -32,7 +32,7 @@ public class Conway {
                     if(/*has 3 living neighbors*/)
                     {
                         //cell comes to life
-                        grid.getGrid()[t][i] = true;
+                        grid.getGrid()[t][i].setAlive(true);
                     } // otherwise the cell stays dead
                 }
             }
@@ -43,38 +43,40 @@ public class Conway {
     }
 
     public boolean getECell(Grid grid, int t, int i) {
-        return grid.getGrid()[t][i+1];
+        return grid.getGrid()[t][i+1].getAlive();
     }
 
     public boolean getNECell(Grid grid, int t, int i) {
-        return grid.getGrid()[t-1][i+1];
+        return grid.getGrid()[t-1][i+1].getAlive();
     }
 
     public boolean getNCell(Grid grid, int t, int i) {
-        return grid.getGrid()[t-1][i];
+        return grid.getGrid()[t-1][i].getAlive();
     }
 
     public boolean getNWCell(Grid grid, int t, int i) {
-        return grid.getGrid()[t-1][i-1];
+        return grid.getGrid()[t-1][i-1].getAlive();
     }
 
     public boolean getWCell(Grid grid, int t, int i) {
-        return grid.getGrid()[t][i-1];
+        return grid.getGrid()[t][i-1].getAlive();
     }
 
     public boolean getSWCell(Grid grid, int t, int i) {
-        return grid.getGrid()[t+1][i-1];
+        return grid.getGrid()[t+1][i-1].getAlive();
     }
 
     public boolean getSCell(Grid grid, int t, int i) {
-        return grid.getGrid()[t+1][i];
+        return grid.getGrid()[t+1][i].getAlive();
     }
 
     public boolean getSECell(Grid grid, int t, int i) {
-        return grid.getGrid()[t+1][i+1];
+        return grid.getGrid()[t+1][i+1].getAlive();
     }
 
     public ArrayList makeNeighborCellsList(boolean currentCell) {
-        List<Boolean> neighborCells = new List<Boolean>(); //You can't make an arraylist of primitive types, can you? Snap!!
+        ArrayList<Cell> neighborCells = new ArrayList<Cell>();
+
+        return neighborCells;
     }
 }
--- src/com/detroitlabs/conwaysgameoflife/ConwaysGameOfLife.java
+++ src/com/detroitlabs/conwaysgameoflife/ConwaysGameOfLife.java
@@ -8,19 +8,37 @@ public class ConwaysGameOfLife {
 
 
         Grid ConwayGrid = new Grid();
-        final boolean _ = false;
-        final boolean X = true;
+//        final boolean _ = false;
+//        final boolean X = true;
 
 
         boolean[][] theSetup = {
-                {_, _, _, _, _},
+                {false, false, false, false, false},
                 {false, false, true, false, false},
                 {false, false, true, false, false},
                 {false, false, true, false, false},
                 {false, false, false, false, false}
         };
 
-        ConwayGrid.setGrid(theSetup);
+        Cell[][] theStartGrid = new Cell[5][5];
+
+
+        for(Cell x: theStartGrid[1]) {
+            x.setAlive(false);
+        }
+
+        for(Cell[] x: theStartGrid) {
+            for (Cell y : x) {
+                y.setAlive(false);
+            }
+            theStartGrid[1][2].setAlive(true);
+            theStartGrid[2][2].setAlive(true);
+            theStartGrid[3][2].setAlive(true);
+        }
+
+        for(int i=0; i<3; )
+
+        ConwayGrid.setGrid(theStartGrid);
 
         int t, i;
         for (t=0; t<5; t++) {
